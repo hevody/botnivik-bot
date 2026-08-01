51bbedc8deea26198f3ce74651ec2e13339b43ad
Velocity <leathergate@proton.me>
2026-08-01 21:17:26 +0800

    feat: .passwordgen, .help

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ## botnivikv2.py
 ![alt-text](./assets/botnivikv2_output.png)
 ![alt-text](./assets/botnivikv2_discord.png)
+
+## botnivikv4.py
+![alt-text](./assets/botnivikv4_output.png)
+![alt-text](./assets/botnivikv4_discord.jpg)
---
+++ botnivikv4.py
@@ -0,0 +1,57 @@
+import discord
+import random
+
+### fetching the discord token ###
+with open('my_bot_token.env') as f:
+  my_bot_token = f.read()
+
+### variable declaration ###
+channel_ids = {
+   'botnivik': 1532586964036092206
+}
+help = {
+  'commands': {
+    'help': ['➡️ `.help`', 'displays this help page'],
+    '.passwordgen': ['➡️ `.passwordgen`', 'generates a 15-character password'],
+    '.': ['➡️ `.`', 'debugs the bot']
+  }
+}
+### 1. initialization of the bot ###
+client = discord.Client(intents=discord.Intents.all())
+
+### 3. bot events ###
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}') 
+
+@client.event
+async def on_message(message):
+  if message.author != client.user:
+    if message.channel.id in channel_ids.values():
+      if message.content == '.passwordgen':
+        ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        digits = '0123456789'
+        punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        candidate_list = []
+        
+        alphanumpunc = ascii_letters + digits + punctuation
+        alphanumpuncList = list(alphanumpunc)
+        for _ in range(15):
+          candidate = random.choice(alphanumpuncList)
+          candidate_list += candidate
+        password = ''.join(candidate_list)
+        embed = discord.Embed(title="Generated password",     # trying embed
+                                      description=password,
+                                      colour=discord.Colour.purple())
+        embed.set_author( name=message.author,
+                          icon_url="https://i.pinimg.com/736x/83/11/1a/83111a294e4eff3e42c905fa21908cbf.jpg" )
+        await message.channel.send(embed=embed)
+      if message.content == '.help':
+        embed = discord.Embed(title='List of all available commands', color=0x49fc03)
+        for i in help['commands'].keys():
+          embed.add_field(name=help['commands'][i][0], value=help['commands'][i][1], inline=False)
+        await message.channel.send(embed=embed)
+
+### 2. running the bot ###
+client.run(my_bot_token)
+
